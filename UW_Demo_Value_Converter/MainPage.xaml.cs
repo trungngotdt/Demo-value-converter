eef0014a6e984a8eea65e94ce814763eadd129b0
trungngotdt <trungngotdt@outlook.com.vn>
2017-06-14 16:28:44 +0700

    commit 1

---
+++ UW_Demo_Value_Converter/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UW_Demo_Value_Converter
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+    public class Age:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            DateTimeOffset? date = value as DateTimeOffset?;
+            if (date==null)
+            {
+                return 0;
+            }
+            return date.Value.Year;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            int year =(int) value;
+            return new DateTime(year, 1, 1);
+        }
+    }
+}
